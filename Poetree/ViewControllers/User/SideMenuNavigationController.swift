5680cba52a187d82e9ebdb03e2aee86039faec98
김동환 <1033513@naver.com>
2021-08-21 18:42:23 +0900

    add side menu

---
+++ Poetree/ViewControllers/User/SideMenuNavigationController.swift
@@ -0,0 +1,29 @@
+//
+//  SideMenuNavigationController.swift
+//  Poetree
+//
+//  Created by 김동환 on 2021/08/21.
+//
+
+import UIKit
+
+class SideMenuNavigationController: UINavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
