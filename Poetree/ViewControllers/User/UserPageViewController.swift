969e0f24253c28a62cca6f87036ed6ed7e5c519b
김동환 <1033513@naver.com>
2021-08-23 03:39:28 +0900

    check push available

--- Poetree/ViewControllers/User/UserPageViewController.swift
+++ Poetree/ViewControllers/User/UserPageViewController.swift
@@ -48,16 +48,7 @@ class UserPageViewController: UIViewController, ViewModelBindable, StoryboardBas
     
     func bindViewModel() {
         
-//        userRegister.rx.tap
-//            .subscribe(onNext:{[unowned self]
-//                _ in
-//                let viewModel = UserRegisterViewModel()
-//                var vc = UserRegisterViewController.instantiate(storyboardID: "UserRelated")
-//                vc.bind(viewModel: viewModel)
-//                self.present(vc, animated: true, completion: nil)
-//
-//            })
-//            .disposed(by: rx.disposeBag)
+
         
         navBarBtn.rx.tap
             .subscribe(onNext:{ [unowned self] _ in
@@ -68,13 +59,7 @@ class UserPageViewController: UIViewController, ViewModelBindable, StoryboardBas
                 
                 
                 present(vc, animated: false, completion: nil)
-//                let sb = UIStoryboard(name: "UserRelated", bundle: nil)
-//
-//                guard let vc = sb.instantiateViewController(identifier: "SideMenuViewController") as? SideMenuViewController else {return}
-//
-//                let nav = SideMenuNavigation(rootViewController: vc)
-//
-//                present(nav, animated: true, completion: nil)
+
                 
             })
             .disposed(by: rx.disposeBag)
