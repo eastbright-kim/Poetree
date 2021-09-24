7ff80b7323f3e34a34a93a3af37f00b878f6a918
김동환 <1033513@naver.com>
2021-09-25 04:36:03 +0900

    before update pods

--- Poetree/Resources/API/Extension.swift
+++ Poetree/Resources/API/Extension.swift
@@ -26,33 +26,3 @@ extension Date {
         return Calendar.current.dateComponents([.weekday, .weekOfYear, .yearForWeekOfYear], from: self).weekday
     }
 }
-
-//
-//extension Date {
-//    static var yesterday: Date { return Date().dayBefore }
-//    static var tomorrow:  Date { return Date().dayAfter }
-//    
-//    var twoWeekBefore: Date {
-//        return Calendar.current.date(byAdding: .day, value: -14, to: noon)!
-//    }
-//    
-//    var aWeekBefore: Date {
-//        return Calendar.current.date(byAdding: .day, value: -7, to: noon)!
-//    }
-//    
-//    var dayBefore: Date {
-//        return Calendar.current.date(byAdding: .day, value: -1, to: noon)!
-//    }
-//    var dayAfter: Date {
-//        return Calendar.current.date(byAdding: .day, value: 1, to: noon)!
-//    }
-//    var noon: Date {
-//        return Calendar.current.date(bySettingHour: 12, minute: 0, second: 0, of: self)!
-//    }
-//    var month: Int {
-//        return Calendar.current.component(.month,  from: self)
-//    }
-//    var isLastDayOfMonth: Bool {
-//        return dayAfter.month != month
-//    }
-//}
